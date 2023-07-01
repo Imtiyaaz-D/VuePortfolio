593ed90ff7f975a6427ff109374df9c3cef4079e
Imtiyaaz-D <imtdamon777@gmail.com>
2023-07-01 13:00:19 +0200

    nav

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <footer id="footer">
+            <div id="copyright">
+                <p>POWERED BY Imti-Damon COPYRIGHT &COPY; {{cYear}}</p>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                cYear: new Date().getUTCFullYear()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
