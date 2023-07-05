a38197cb2f18d59e790ea6bf34ef1527586474d1
Imtiyaaz-D <imtdamon777@gmail.com>
2023-07-05 16:28:29 +0200

    aboutpg

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>About</h2>
+    <div class="about-text">
+      <img src="https://i.postimg.cc/k5DXHknx/C12-C2-Mogammad-Damon-1-removebg-preview.png" alt="profile image" loading="lazy" id="profile image">
+      <p id="profile-text">I have always been intrested in techology / coding from young age.The techology in the world has always fascinated me and coding especially has a special place in my heart,even though it can be challenging i am willing to work through any hardships that comes my way. </p>
+    </div>
+   <a href="../assets/cv/CURRICULUM_VITAE_Mogammad_Imtiyaaz_Damon_without_cover_letter (1).docx"><button>Download CV</button></a>
   </div>
 </template>
 
